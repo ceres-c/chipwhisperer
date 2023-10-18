1b0decb8824fb6a2c7bd3839fe41188a32abb2c2
Federico Cerutti <federico@ceres-c.it>
2023-10-18 20:09:20 +0200

    UNTESTED code for arm and capture

--- software/chipwhisperer/capture/scopes/MSO4.py
+++ software/chipwhisperer/capture/scopes/MSO4.py
@@ -9,14 +9,23 @@ class MSO4:
         rm: pyvisa.ResourceManager instance
         sc: pyvisa.resources.MessageBasedResource instance
         trig: MSO4Triggers type (not an instance)
+        src (src): Source for the waveform data (e.g. 'CH1')
     """
 
     _name = "ChipWhisperer/MSO4"
+    sources = ['ch1', 'ch2', 'ch3', 'ch4'] # TODO add MATH_, REF_, CH_D...
+    # See programmer manual § DATa:SOUrce
 
     def __init__(self):
         self.rm: pyvisa.ResourceManager = None # type: ignore
         self.sc: pyvisa.resources.MessageBasedResource = None # type: ignore
-        self._trig: MSO4Triggers = None # type: ignore # Local storage for the internal trigger instance
+
+        self.record_len = 0
+
+        # Local storage for the internal trigger instance
+        self._trig: MSO4Triggers = None # type: ignore
+
+        self._src: str = None # type: ignore
 
         self.connectStatus = False
 
@@ -75,6 +84,19 @@ class MSO4:
         self.sc.write('DESE 255')
         # Clear: Event Queue, Standard Event Status Register, Status Byte Register
         self.sc.write('*CLS')
+        # Set waveform data format to binary (faster than ASCII)
+        self.sc.write('DATa:ENCdg RFBinary') # float msb first
+        # Set waveform data to 32-bit
+        self.sc.write('WFMOutpre:BYT_Nr 4')
+        # Get the record length
+        try:
+            self.record_len = int(self.sc.query('HORizontal:MODe:RECOrdlength?').strip())
+        except Exception:
+            self.dis()
+            raise
+        # Set data start and stop (retrieve all data)
+        self.sc.write('DATA:START 1')
+        self.sc.write(f'DATA:STOP {self.record_len}')
 
         self.connectStatus = True
         return True
@@ -104,21 +126,33 @@ class MSO4:
 
         if self.connectStatus is False:
             raise OSError("Scope is not connected. Connect it first...")
-        
+
         try:
-            pass
+            self.sc.write('ACQuire:STATE 1') # Acquire one trace
         except Exception:
             self.dis()
             raise
 
-        pass
-
     def capture(self):
         """Reads power trace data from the scope.
-        
+
         Returns:
             False upon success and True upon failure
         """
+
+        if not self._src:
+            raise ValueError("Must set waveform source before starting capture")
+
+        available = self.sc.query('DATa:SOUrce:AVAILable?').lower()
+        if 'none' in available:
+            return False
+
+        # Read the preamble
+        preamble = self.sc.query('WFMOutpre').strip() # TODO how to use this?
+
+        # Read the data
+        data = self.sc.query('CURVE?').strip()
+
         pass
 
     def get_last_trace(self):
@@ -135,3 +169,13 @@ class MSO4:
     @trig.setter
     def trig(self, trig_type: MSO4Triggers):
         self._trig = trig_type(self.sc)
+
+    @property
+    def src(self) -> str:
+        return self.sc.query('DATa:SOUrce?').strip()
+    @src.setter
+    def src(self, src: str):
+        if src.lower() not in self.sources:
+            raise ValueError(f"Invalid source {src}. Valid sources are {self.sources}")
+        self._src = src
+        self.sc.write(f'DATa:SOUrce {src}')
