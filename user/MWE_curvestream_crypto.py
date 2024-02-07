5af57173d5a28177649acfa9451b5f0d2270da75
Federico Cerutti <federico@ceres-c.it>
2024-02-07 17:57:08 +0100

    Improved script reliability

--- user/MWE_curvestream_crypto.py
+++ user/MWE_curvestream_crypto.py
@@ -4,13 +4,12 @@ import time
 
 TIMEOUT = 20000
 TIMEOUT_SHORT = 200 # Used when running the acquisition loop and don't want to waste time on a missed trigger
+TRACES = 1000
 
 print("set up VISA connection")
 
 rm = visa.ResourceManager()
-
 scope_address = "TCPIP0::192.168.1.140::inst0::INSTR"
-
 scope: visa.resources.MessageBasedResource = rm.open_resource(scope_address) # type: ignore
 scope.timeout = TIMEOUT
 
@@ -37,7 +36,8 @@ scope.write("TRIGger:A:LEVEL:CH1 1.4")
 scope.write("TRIGger:A:EDGE:SLOPe RISE")
 scope.write("TRIGger:A:MODe NORMAL")
 
-scope.write("ACQuire:FASTAcq:STATE ON")
+# Enable fast acquisition mode and curvestream
+scope.write("ACQuire:FASTAcq:STATE ON") # NOTE: Interestingly enough, if FASTAcq is not enabled the scope UI will DIE ON YOU. Tektronix is a meme.
 scope.write("CURVEStream?")
 
 # Configure the target
@@ -49,16 +49,12 @@ target = cw.target(None, cw.targets.CW305, force=True, fpga_id=fpga_id)
 # run at 10 MHz:
 target.pll.pll_outfreq_set(10E6, 1)
 
-# Acquire 1000 traces
 traces = []
-skipped = False # Used to simulate a "glitch" in the trigger signal
-# scope.write("display:waveform 1")
-# Enable curvestream mode
 scope.timeout = TIMEOUT_SHORT # Reduce timeout here in case we miss a trigger
-for i in range(100):
+start = time.time()
+for i in range(TRACES):
     print(i, end="\r", flush=True)
-    if not (i == 32 and not skipped):
-        skipped = True
+    if i != 32: # Emulate a missed trigger
         target.simpleserial_write('p', b'\x00' * 16)
         response = target.simpleserial_read('r', target.output_len, ack=True)
     try:
@@ -66,12 +62,17 @@ for i in range(100):
     except visa.errors.VisaIOError as e:
         if e.error_code == visa.constants.VI_ERROR_TMO:
             print("Timeout occurred")
-            i -= 1
+            # Reinit curvestream
+            scope.write("*CLS")
+            scope.write("CURVEStream?")
             continue
     if traces and trace == traces[-1]:
         print("Duplicate trace detected")
     traces.append(trace)
 scope.timeout = TIMEOUT # Reset timeout
+end = time.time()
+print(f"Captured {len(traces)} traces in {end - start} seconds")
 
 print(len(traces))
+scope.write("*RST")
 scope.write("*CLS")
